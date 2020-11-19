c602378731a45faddb3aacf67ee7565150a3097a
Lingvo28 <60513523+Lingvo28@users.noreply.github.com>
2020-11-19 12:10:40 +0200

    first commit

---
+++ SpacEng/assets/js/script.js
@@ -0,0 +1,12 @@
+let popup = document.getElementById('mypopup'),
+    popupToggle = document.getElementById('btn--popup'),
+    popupBg = document.getElementById('bg');
+    
+
+popupToggle.onclick = function(){
+    popup.style.display = "block";
+}
+
+bg.onclick = function(){
+    popup.style.display = "none";
+}
